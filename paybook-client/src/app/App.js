e70190c1bf2bd0a3ab2406578fc4dfc5a608cc4b
jaystar13 <astarisk2@gmail.com>
2023-02-19 18:04:35 +0900

    feat: add front-end initial frame

---
+++ paybook-client/src/app/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import AppHeader from "../common/AppHeader";
+
+import { Layout } from "antd";
+
+export default function App() {
+  return (
+    <Layout className="app-container">
+      <AppHeader></AppHeader>
+    </Layout>
+  );
+}
---
+++ paybook-client/src/common/AppHeader.js
@@ -0,0 +1,34 @@
+import "./AppHeader.css";
+
+import { Layout, Menu } from "antd";
+import { Link } from "react-router-dom";
+const Header = Layout.Header;
+
+export default function AppHeader() {
+  const menuItems = [
+    {
+      label: "Login",
+      key: "login",
+    },
+    {
+      label: "Signin",
+      key: "signin",
+    },
+  ];
+
+  return (
+    <Header className="app-header">
+      <div className="container">
+        <div className="app-title">
+          <Link to="/">Paybook App</Link>
+        </div>
+        <Menu
+          className="app-menu"
+          mode="horizontal"
+          style={{ lineHeight: "64px" }}
+          items={menuItems}
+        ></Menu>
+      </div>
+    </Header>
+  );
+}
