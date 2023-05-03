379346801c8e03c0b69e82b5fb205fe07e850d78
lee.jaesung <lee.jaesung@joins.com>
2023-05-03 14:33:22 +0900

    test: delete no usage class

--- paybook-server/src/test/java/com/paybook/common/CommonApiTest.java
+++
@@ -1,7 +0,0 @@
-package com.paybook.common;
-
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan(basePackages = {"com.paybook.security", "com.paybook.config", "com.paybook.user.repository"})
-public class CommonApiTest {
-}
